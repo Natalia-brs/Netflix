59cb1f494d8006650c301991f465461acddc376e
natalia <ntla.brs@gmail.com>
2023-04-08 16:25:55 -0300

    fazendo o fetch dos trailers

--- netflix-app/src/components/Modal.tsx
+++ netflix-app/src/components/Modal.tsx
@@ -1,14 +1,30 @@
 import MuiModal from '@mui/material/Modal';
 import { useRecoilState } from 'recoil';
-import { modalState } from '@/atoms/modalAtoms';
+import { modalState, movieState } from '@/atoms/modalAtoms';
 import { HiX } from "react-icons/hi";
+import { useEffect, useState } from 'react';
+import { Genre, Movie, Element, } from '../../typing';
 
 function Modal() {
     const [showModal, setShowModal] = useRecoilState(modalState);
+    const [movie, setMovie] = useRecoilState(movieState);
+
+ useEffect(() => {
+  if(!movie) return
+   async function fetchMovie() {
+    const data = await fetch(`https://api.themoviedb.org/3/${
+      movie?.media_type === 'tv' ? 'tv' : 'movie'
+    }/${movie?.id}?api_key=${
+      process.env.NEXT_PUBLIC_API_KEY
+    }&language=en-US&append_to_response=videos`)
+   }
+ }, []).then((response) => response.json());
+  
 
 const handleClose = () => {
     setShowModal(false)
 }
+
   return (
     <MuiModal open= { showModal } onClose={ handleClose }> 
         <>
@@ -18,6 +34,10 @@ const handleClose = () => {
         >
           <HiX className="h-6 w-6" />
         </button>
+
+        <div>
+
+        </div>
         </>
     </MuiModal>
   )
