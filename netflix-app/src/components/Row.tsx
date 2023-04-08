3994a467ee627c2568b45eaa5189e1ddfc1fcbdf
natalia <ntla.brs@gmail.com>
2023-04-08 01:52:43 -0300

    adicionando o scroll

--- netflix-app/src/components/Row.tsx
+++ netflix-app/src/components/Row.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '../../typing';
 import { HiOutlineChevronDoubleLeft, HiOutlineChevronDoubleRight } from "react-icons/hi2";
 import Thumbnail from './Thumbnail';
+import { useRef, useState } from 'react';
 
 interface Props {
     title: string
@@ -8,6 +9,21 @@ interface Props {
   }
 
 function Row({ title, movies }: Props) {
+    const rowRef = useRef<HTMLDivElement>(null);
+    const[isMoved, setIsMoved] = useState(false);
+
+    const handleClick = (direction: string) => {
+        setIsMoved(true);
+       if(rowRef.current) {
+        const { scrollLeft, clientWidth } = rowRef.current;
+
+        const scrollTo = direction === 'left' 
+        ? scrollLeft - clientWidth 
+        : scrollLeft + clientWidth
+        rowRef.current.scrollTo({left: scrollTo, behavior: "smooth"});
+       };
+    }
+    
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
         <h2 className="w-56 cursor-pointer text-sm font-semibold  text-[#e5e5e5]
@@ -15,10 +31,15 @@ function Row({ title, movies }: Props) {
             { title }
             </h2>
         <div className="group relative md:-ml-2">
-          <HiOutlineChevronDoubleLeft className=' absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 
-           cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100' />
+          <HiOutlineChevronDoubleLeft 
+          onClick={() => handleClick("left")}
+           className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`} />
 
-           <div  className="flex items-center space-x-0.5 
+           <div 
+           ref={ rowRef }
+           className="flex items-center space-x-0.5 
            overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
 
            { movies.map((movie)=> (
@@ -26,7 +47,9 @@ function Row({ title, movies }: Props) {
            ))}
            </div>
 
-           <HiOutlineChevronDoubleRight className=' absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 
+           <HiOutlineChevronDoubleRight 
+           onClick={() => handleClick("right")}
+           className=' absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 
            cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100'/>
         </div>
     </div>
